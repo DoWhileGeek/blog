9d9419300ce0217ad1bc44ad11b02738552a88a2
Joeseph Rodrigues <dowhilegeek@gmail.com>
2019-04-21 01:52:00 -0700

    make a post template and generate posts with it

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const slugify = require('slugify')
+const moment = require('moment')
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      const { title, date } = node.frontmatter
+
+      createPage({
+        path: `${moment(date).format('YYYY/M/D')}/${slugify(title)}/`,
+        component: blogPostTemplate,
+        context: {title, date}, // additional data can be passed via context
+      })
+    })
+  })
+}
