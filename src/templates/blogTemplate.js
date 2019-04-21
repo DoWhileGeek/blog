c3da2e6a6b0084d181855c2361de00476ba2ca8c
Joeseph Rodrigues <dowhilegeek@gmail.com>
2019-04-21 15:16:24 -0700

    add material ui

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,23 +1,41 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Paper from '@material-ui/core/Paper'
+// import CardActions from '@material-ui/core/CardActions'
+// import Button from '@material-ui/core/Button'
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
+  const { title, date } = frontmatter
+
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+    <div style={s.container} className="blog-post-container">
+      <Paper style={s.paper} elevation={4}>
+        <h1>{title}</h1>
+        <h3>{date}</h3>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
-      </div>
+      </Paper>
     </div>
   )
+}
+
+const s = {
+  container: {
+    padding: '20px',
+    margin: '0 auto',
+    width: '80%'
+  },
+
+  paper: {
+    padding: '20px',
+  }
 }
 
 export const pageQuery = graphql`
