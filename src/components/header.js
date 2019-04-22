3ee7fd56be174bd7fffeddbe1384d7255fd8cfe6
Joeseph Rodrigues <dowhilegeek@gmail.com>
2019-04-22 12:38:08 -0700

    remove extra header element

--- src/components/header.js
+++ src/components/header.js
@@ -7,15 +7,13 @@ import Toolbar from '@material-ui/core/Toolbar'
 import styles from '../styles/components/header.module.css'
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <AppBar>
-      <Toolbar>
-        <Link to="/" className={styles.titleLink}>
-          <h1>{siteTitle}</h1>
-        </Link>
-      </Toolbar>
-    </AppBar>
-  </header>
+  <AppBar position="static">
+    <Toolbar>
+      <Link to="/" className={styles.titleLink}>
+        <h1>{siteTitle}</h1>
+      </Link>
+    </Toolbar>
+  </AppBar>
 )
 
 Header.defaultProps = {
